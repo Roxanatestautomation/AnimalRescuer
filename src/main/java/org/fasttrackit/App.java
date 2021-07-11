a96af7f488908da1ad6af558411e39ea0e6f9196
Roxana <roxanacroitoru378@yahoo.ro>
2021-07-11 12:06:03 +0100

    practice with arrays/for_loops/lists

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -35,6 +35,8 @@ public class App
         game.setAnimal(animal);
         game.setMedic(medic);
 
+        game.start();
+
         rescuer.feed(animal,food);
         rescuer.entertain(animal, activity);
 
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,10 +1,65 @@
 package org.fasttrackit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
     private Rescuer rescuer;
     private Animal animal;
     private MedicalStaff medic;
+
+    private List<Food> availableFood = new ArrayList<>();
+    private EntertainmentActivity[] availableActivities = new EntertainmentActivity[10];
+
+    public List<Food> getAvailableFood() {
+        return availableFood;
+    }
+
+    public EntertainmentActivity[] getAvailableActivities() {
+        return availableActivities;
+    }
+
+    private void initFood() {
+        Food dogFood = new Food("Purina");
+        Food fishFood = new Food("Tetra");
+        availableFood.add(dogFood);
+        availableFood.add(fishFood);
+    }
+
+    private void initActivities(){
+        EntertainmentActivity activity1 = new EntertainmentActivity("Running");
+        EntertainmentActivity activity2 = new EntertainmentActivity("Swimming");
+        availableActivities[0] = activity1;
+        availableActivities[1] = activity2;
+    }
+
+    public void start() {
+        initFood();
+        initActivities();
+
+        displayAvailableFood();
+        displayAvailableActivities();
+    }
+
+    private void displayAvailableFood() {
+        System.out.println("Available food:");
+        for (Food food : availableFood) {
+            if (food != null) {
+                System.out.println(food.getName());
+            }
+        }
+    }
+
+    private void displayAvailableActivities() {
+        System.out.println("Available activities:");
+        for (int i = 0; i < availableActivities.length; i++) {
+            if (availableActivities[i] != null) {
+                System.out.println(availableActivities[i].getName());
+            }
+        }
+    }
+
 
     public Rescuer getRescuer() {
         return rescuer;
